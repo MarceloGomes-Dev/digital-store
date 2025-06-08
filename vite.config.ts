b9b3780012779e7d2779dbebd69e3077dacae6f6
marcelogomes <marceloag57@gmail.com>
2025-06-08 08:38:22 -0300

    tentativa deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/digital-store/',
-})
+  build: {
+    rollupOptions: {
+      external: ['react-router'], // Adicione esta linha
+    },
+  },
+});
