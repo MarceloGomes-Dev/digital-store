2249724e99d42be546ae8fc91e168fcbabda4111
marcelogomes <marceloag57@gmail.com>
2025-06-09 08:11:12 -0300

    deploy test

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/digital-store/', // 👈 base igual ao nome do repositório
   plugins: [react()],
-  base: '/digital-store/',
 })
-
